8db7b6483492d94cd0280dc98664a81f35aa5377
maochoa91 <maochoa91@hotmail.com>
2022-10-15 09:50:11 -0500

    inicio front

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route } from "react-router-dom";
+import { BrowserRouter,Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 
@@ -8,12 +8,17 @@ function App() {
 
 <div className="App">
 
+ <BrowserRouter>
+      <Route exact path="/" component={Home} />
+      <Route>
+        <Navbar></Navbar>
 
-<Navbar></Navbar>
+      </Route>
+ </BrowserRouter>
 
- 
- <Home></Home>     
+
 </div>
+
    
       
   );
